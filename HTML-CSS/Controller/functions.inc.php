03252183d6134676d6ea6c67e0efc6f6e4178c9e
Sh0lf <vyvincentyap@gmail.com>
2022-12-07 11:19:47 +0100

    update: updating stucture

--- HTML-CSS/Controller/functions.inc.php
+++ HTML-CSS/Controller/functions.inc.php
@@ -1,5 +1,7 @@
 <?php
 
+require '../Model/SQL-Functions.php';
+
 function EmptyInputSignup($username, $pwd, $pwdrep, $nom, $prenom, $email, $phone, $sexe){
     $result = "";
     if (empty($username) || empty($$pwd) || empty($pwdrep) || empty($nom) || empty($prenom) || empty($email) || empty($phone) || empty($sexe)) {
@@ -40,46 +42,6 @@ function pwdMatch($pwd, $pwdrep){
     return $result;
 }
 
-function uidExists($conn, $username, $email){
-    $sql = "SELECT * FROM User WHERE Username = ? OR Email = ?;";
-    $stmt = mysqli_stmt_init($conn);
-    if (!mysqli_stmt_prepare($stmt, $sql)) {
-        header("location: ../Views/inscription.php?error=stmtfailed");
-        exit();
-    }
-
-    mysqli_stmt_bind_param($stmt, "ss", $username, $email);
-    mysqli_stmt_execute($stmt);
-
-    $resultData = mysqli_stmt_get_result($stmt);
-
-    if ($row = mysqli_fetch_assoc($resultData)){
-        return $row;
-    }
-    else {
-        $result = false;
-        return $result;
-    }
-
-    mysqli_stmt_close($stmt);
-}
-
-function createUser($conn, $username, $nom, $prenom, $email, $phone, $sexe, $pwd, $SpeMed, $role){
-    $sql = "INSERT INTO user (username, familyname, name, email, phone, sexe, password, speMed, role) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);";
-    $stmt = mysqli_stmt_init($conn);
-    if (!mysqli_stmt_prepare($stmt, $sql)) {
-        header("location: ../Views/inscription.php?error=stmtfailed");
-        exit();
-    }
-
-    $hashedpwd = password_hash($pwd, PASSWORD_BCRYPT);
-
-    mysqli_stmt_bind_param($stmt, "ssssissss", $username, $nom, $prenom, $email, $phone, $sexe, $hashedpwd, $SpeMed, $role);
-    mysqli_stmt_execute($stmt);
-    mysqli_stmt_close($stmt);
-    header("location: ../Views/inscription.php?error=none");
-    exit();
-}
 
 function EmptyInputLogin($username, $pwd){
     $result = "";
