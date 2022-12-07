dab900994ce80a98e7645ee46e50aa02cf65b66f
Sh0lf <vyvincentyap@gmail.com>
2022-12-07 11:22:15 +0100

    updating: added 1 line require

--- HTML-CSS/Controller/login.inc.php
+++ HTML-CSS/Controller/login.inc.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 ini_set('display_errors', 1);
 include_once './dbh.inc.php';
 include_once './functions.inc.php';
@@ -10,18 +9,6 @@ if (isset($_POST["submit"])){
 
     if (EmptyInputLogin($username, $pwd) !== false){
         header("location: ../views/loginsys/login.php?error=emptyinput");
-=======
-
-if (isset($_POST["submit"])){
-    $username = $POST["username"];
-    $pwd = $POST["pwd"];
-
-    require_once 'dbh.inc.php';
-    require_once 'functions.inc.php';
-
-    if (EmptyInputLogin($username, $pwd) !== false){
-        header("location: ../Views/login.php?error=emptyinput");
->>>>>>> 28487d0 (update php MVC partiel login system)
         exit();
     }
 
@@ -29,14 +16,8 @@ if (isset($_POST["submit"])){
 }
 
 else {
-<<<<<<< HEAD
     header("location:../views/loginsys/login.php");
     exit();
 }
 
 ?>
-=======
-    header("location:../Views/login.php");
-    exit();
-}
->>>>>>> 28487d0 (update php MVC partiel login system)
--- HTML-CSS/Controller/logout.inc.php
+++ HTML-CSS/Controller/logout.inc.php
@@ -1,14 +1,8 @@
 <?php
-<<<<<<< HEAD
 ini_set('display_errors', 1);
     session_start();
     session_unset();
     session_destroy();
 
     header("location:../views/mainmenu.php")
-=======
-    session_start();
-    session_unset();
-    session_destroy();
->>>>>>> 57da51f (update:php)
 ?>
