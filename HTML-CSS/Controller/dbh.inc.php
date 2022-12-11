b3bef54af797e87f7324210bed7345131dc29619
Sh0lf <vyvincentyap@gmail.com>
2022-12-11 15:50:23 +0100

    some modifications

--- HTML-CSS/Controller/dbh.inc.php
+++ HTML-CSS/Controller/dbh.inc.php
@@ -2,8 +2,8 @@
 
 $conn = new mysqli("85.159.213.31", "adminG5B", "adminG5B", "Projet_BDD_APP");
 
-if ($mysqli -> connect_errno) {
-    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+if ($conn -> connect_errno) {
+    echo "Failed to connect to MySQL: " . $conn -> connect_error;
     exit();
 }
 
