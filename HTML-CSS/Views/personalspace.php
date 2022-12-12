76a312c53a7957239bcbfd83f909efd98d2c06df
Sh0lf <vyvincentyap@gmail.com>
2022-12-13 00:13:21 +0100

    updating + changes. still need to check FK error

---
+++ HTML-CSS/Views/personalspace.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <meta charset="UTF-8">  
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <link rel="stylesheet" href="navbar.css">
+    <link rel="stylesheet" href="personalspace.css">
+  </head>
+
+  <body>
+    <header>
+      <!-- Defining in header a top navigation bar-->
+      <?php
+        include_once 'header.php';
+      ?>
+    </header>
+
+    <div class="container">
+
+    </div>
+
+
+    <footer>
+        <!--Defining our bottom navigation bar in footer, for aesthetics purpose-->
+        <?php
+        include_once 'footer.php';
+        ?>
+    </footer>
+  </body>
+</html>
