9f1b93cc7f88c6427a399dfa63fe13ac6ab3ea1c
Sh0lf <vyvincentyap@gmail.com>
2022-12-12 14:16:39 +0100

    Updating on clarity

--- HTML-CSS/Controller/logout.inc.php
+++ HTML-CSS/Controller/logout.inc.php
@@ -4,5 +4,9 @@ ini_set('display_errors', 1);
     session_unset();
     session_destroy();
 
+<<<<<<< HEAD
     header("location:../views/mainmenu.php")
+=======
+    header("location:../Views/index.php")
+>>>>>>> fe02d72 (Updating on clarity)
 ?>
--- HTML-CSS/Model/SQL-loginsystem.php
+++ HTML-CSS/Model/SQL-loginsystem.php
@@ -1,5 +1,6 @@
 <?php
 
+<<<<<<< HEAD
 ini_set('display_errors', 1);
 include_once '../controller/functions.inc.php';
 include_once '../controller/dbh.inc.php';
@@ -177,4 +178,55 @@ function changePwd($conn, $pwd, $code){
     return true;
 }
 
+=======
+<<<<<<< HEAD
+require '../Controller/functions.inc.php';
+=======
+include_once '../Controller/functions.inc.php';
+include_once '../Controller/dbh.inc.php';
+>>>>>>> 06adc99 (small update; some modifications)
+
+function uidExists($conn, $username, $email){
+    $sql = "SELECT * FROM user_test WHERE username = ? OR email = ?;";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: ../Views/inscription.php?error=stmtfailed");
+        exit();
+    }
+
+    mysqli_stmt_bind_param($stmt, "ss", $username, $email);
+    mysqli_stmt_execute($stmt);
+
+    $resultData = mysqli_stmt_get_result($stmt);
+    $row = mysqli_fetch_assoc($resultData);
+
+    mysqli_stmt_close($stmt);
+
+    if ($row){
+        return $row;
+    }
+    else {
+        $result = false;
+        return $result;
+    }
+}
+
+function createUser($conn, $username, $nom, $prenom, $email, $phone, $sexe, $pwd, $role){
+    $sql = "INSERT INTO user_test (username, familyname, name, email, phone, sexe, password, role) VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: ../Views/inscription.php?error=stmtfailed");
+        exit();
+    }
+
+    $hashedpwd = password_hash($pwd, PASSWORD_DEFAULT);
+
+    mysqli_stmt_bind_param($stmt, "ssssisss", $username, $nom, $prenom, $email, $phone, $sexe, $hashedpwd, $role);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+    header("location: ../Views/inscription.php?error=none");
+    exit();
+}
+
+>>>>>>> fe02d72 (Updating on clarity)
 ?>
