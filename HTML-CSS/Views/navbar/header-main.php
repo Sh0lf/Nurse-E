13714fbc2402bc7ff8b1a23f7a0d4405d3a25471
Sh0lf <vyvincentyap@gmail.com>
2022-12-14 23:01:34 +0100

    made navbar based on if session exists (help me)

--- HTML-CSS/Views/navbar/header-main.php
+++ HTML-CSS/Views/navbar/header-main.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <nav>  
     <?php
     session_start();  
@@ -8,6 +9,10 @@
         echo '<img class="logo" src="logo.png">';
     }
     ?> 
+=======
+<nav>
+    <img class="logo" src="logo.png">
+>>>>>>> d9a6817 (made navbar based on if session exists (help me))
     <label for="btn" class="icon">
         <span class="fa fa-bars"></span>
     </label>
@@ -15,6 +20,7 @@
     <ul>
         <?php
             if (isset($_SESSION["username"])){
+<<<<<<< HEAD
                 echo '<li><a href="http://nurse-medicobot.wstr.fr/views/mainmenu.php">Accueil</a></li>';
             } else {
             echo '<li>
@@ -26,18 +32,38 @@
                         <li><a href="http://nurse-medicobot.wstr.fr/views/mainmenu.php#produit">Notre produit</a></li>
                         <li><a href="http://nurse-medicobot.wstr.fr/views/mainmenu.php#updates">Les updates</a></li>
                         <li><a href="http://nurse-medicobot.wstr.fr/views/mainmenu.php#questions">Vos questions</a></li>                                
+=======
+                echo '<li><a href="#accueil">Accueil</a></li>';
+            } else {
+            echo '<li>
+                    <label for="btn-1" class="show">Accueil +</label>
+                    <a href="#home">Accueil <i class="fas fa-caret-down"></i></a>
+                    <input type="checkbox" id="btn-1">
+                    <ul>
+                        <li><a href="#">Comment ça marche ?</a></li>
+                        <li><a href="#">Notre produit</a></li>
+                        <li><a href="#">Les updates</a></li>
+                        <li><a href="#">Vos questions</a></li>                                
+>>>>>>> d9a6817 (made navbar based on if session exists (help me))
                     </ul>
                 </li>';
             }
         ?>
+<<<<<<< HEAD
         <?php
         if(isset($_SESSION["username"])) {
             echo '<li><label for="btn-2" class="show">MédicoBot +</label>
+=======
+        
+        <li>
+            <label for="btn-2" class="show">MédicoBot +</label>
+>>>>>>> d9a6817 (made navbar based on if session exists (help me))
             <a href="#produits">MédicoBot <i class="fas fa-caret-down"></i></a>
             <input type="checkbox" id="btn-2">
             <ul>
                 <li><a href="#">Vos analyses</a></li>
                 <li><a href="#">Votre dossier</a></li>
+<<<<<<< HEAD
             </ul></li>';
         } else {
             echo '<li><a href="http://nurse-medicobot.wstr.fr/views/medicalbot/medicobot.php">Médicobot</a></li>';
@@ -69,6 +95,17 @@
                     </ul></li>';
         } else {
             echo '<li><a href="http://nurse-medicobot.wstr.fr/views/loginsys/login.php"><button class="bouton">Connexion</button></a></li>';
+=======
+            </ul>    
+        </li>      
+        <li><a href="#questions">Ecologie</a></li>
+        <li><a href="#apropos">A propos</a></li>
+        <?php
+        if(isset($_SESSION["username"])) {
+            echo '<li><button href="#profil">Mon profil</button></li>';
+        } else {
+            echo '<li><button class="bouton" href="../loginsys/login.php">Connexion</button></li>';
+>>>>>>> d9a6817 (made navbar based on if session exists (help me))
         }
         ?>
     </ul>
