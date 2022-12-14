6767e02ee791541c27ec1ad62bd3a123ed316ab8
Sh0lf <vyvincentyap@gmail.com>
2022-12-14 14:23:30 +0100

    update login sys views (help me)

--- HTML-CSS/Views/navbar/header-ecologie-notlogged.php
+++ HTML-CSS/Views/navbar/header-ecologie-notlogged.php
@@ -4,36 +4,36 @@
         <span class="fa fa-bars"></span>
     </label>
     <input type="checkbox" id="btn">
-        <ul>
-            <li>
-                <label for="btn-1" class="show">Accueil +</label>
-                <a href="#home">Accueil <i class="fas fa-caret-down"></i></a>
-                <input type="checkbox" id="btn-1">
-                <ul>
-                    <li><a href="#">Comment ça marche ?</a></li>
-                    <li><a href="#">Notre produit</a></li>
-                    <li><a href="#">Les updates</a></li>
-                    <li><a href="#">Vos questions</a></li>                                
-                </ul>
-            </li>
-            <li>
-                <label for="btn-2" class="show">MédicoBot +</label>
-                <a href="#produits">MédicoBot <i class="fas fa-caret-down"></i></a>
-                <input type="checkbox" id="btn-2">
-                <ul>
-                    <li><a href="#">Vos analyses</a></li>
-                    <li><a href="#">Votre dossier</a></li>
-                </ul>    
-            </li>      
-            <li>
-                <label for="btn-3" class="show">Ecologie +</label>
-                <a href="eco">Ecologie <i class="fas fa-caret-down"></i></a>
-                <input type="checkbox" id="btn-3">
-                <ul>
-                    <li><a href="#">Votre arbre</a></li>
-                </ul>
-            </li>
-            <li><a href="#apropos">A propos</a></li>
-            <li><button class="bouton">Mon profil</button></li>
-        </ul>
-    </nav>
+    <ul>
+        <li>
+            <label for="btn-1" class="show">Accueil +</label>
+            <a href="#home">Accueil <i class="fas fa-caret-down"></i></a>
+            <input type="checkbox" id="btn-1">
+            <ul>
+                <li><a href="#">Comment ça marche ?</a></li>
+                <li><a href="#">Notre produit</a></li>
+                <li><a href="#">Les updates</a></li>
+                <li><a href="#">Vos questions</a></li>                                
+            </ul>
+        </li>
+        <li>
+            <label for="btn-2" class="show">MédicoBot +</label>
+            <a href="#produits">MédicoBot <i class="fas fa-caret-down"></i></a>
+            <input type="checkbox" id="btn-2">
+            <ul>
+                <li><a href="#">Vos analyses</a></li>
+                <li><a href="#">Votre dossier</a></li>
+            </ul>    
+        </li>      
+        <li>
+            <label for="btn-3" class="show">Ecologie +</label>
+            <a href="eco">Ecologie <i class="fas fa-caret-down"></i></a>
+            <input type="checkbox" id="btn-3">
+            <ul>
+                <li><a href="#">Votre arbre</a></li>
+            </ul>
+        </li>
+        <li><a href="#apropos">A propos</a></li>
+        <li><button class="bouton">Mon profil</button></li>
+    </ul>
+</nav>
