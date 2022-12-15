f3968e09e2adc5fbf0919ece3b18da632bbdf0b3
Sh0lf <vyvincentyap@gmail.com>
2022-12-15 16:40:50 +0100

    changements updates help me rompiche

--- HTML-CSS/Views/navbar/footer.php
+++ HTML-CSS/Views/navbar/footer.php
@@ -1,6 +1,13 @@
 <div class="bottombar">
+<<<<<<< HEAD
     <a href="http://nurse-medicobot.wstr.fr/views/misc/cgu.php">CGU</a>
     <a href="http://nurse-medicobot.wstr.fr/views/misc/FAQ.php">FAQ</a>
     <a href="http://nurse-medicobot.wstr.fr/views/misc/apropos.php">A propos de</a> 
     <a href="http://nurse-medicobot.wstr.fr/views/misc/contact.php">Contact</a>
+=======
+    <a href="#cgu">CGU</a>
+    <a href="../misc/FAQ.php">FAQ</a>
+    <a href="../misc/apropos.php">A propos de</a> 
+    <a href="#contact">Contact</a>
+>>>>>>> 63b7206 (changements updates help me rompiche)
 </div>
