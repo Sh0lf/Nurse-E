63b7206354c178b5091912f9e1579b3af3d91c11
Sh0lf <vyvincentyap@gmail.com>
2022-12-15 16:40:50 +0100

    changements updates help me rompiche

--- HTML-CSS/Views/navbar/footer.php
+++ HTML-CSS/Views/navbar/footer.php
@@ -1,6 +1,6 @@
 <div class="bottombar">
     <a href="#cgu">CGU</a>
-    <a href="#faq">FAQ</a>
-    <a href="#aproposde">A propos de</a> 
+    <a href="../misc/FAQ.php">FAQ</a>
+    <a href="../misc/apropos.php">A propos de</a> 
     <a href="#contact">Contact</a>
 </div>
