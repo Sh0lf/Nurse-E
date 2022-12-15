e91a296aa544790048d402f39045e587c33f60fd
Sh0lf <vyvincentyap@gmail.com>
2022-12-15 15:32:43 +0100

    changes updated navbar

--- HTML-CSS/Views/navbar/header-main.php
+++ HTML-CSS/Views/navbar/header-main.php
@@ -11,8 +11,20 @@
     ?> 
 =======
 <nav>
+<<<<<<< HEAD
     <img class="logo" src="logo.png">
 >>>>>>> d9a6817 (made navbar based on if session exists (help me))
+=======
+    <?php  
+    $curPageName = substr($_SERVER["SCRIPT_NAME"],strrpos($_SERVER["SCRIPT_NAME"],"/")+1);  
+    if($curPageName = "ecologie.php") {
+        echo '<img class="logo" src="nurse.png">';
+    }  else {
+        echo '<img class="logo" src="logo.png">';
+    }
+    ?>   
+    
+>>>>>>> 31cd901 (changes updated navbar)
     <label for="btn" class="icon">
         <span class="fa fa-bars"></span>
     </label>
@@ -50,6 +62,7 @@
             }
         ?>
 <<<<<<< HEAD
+<<<<<<< HEAD
         <?php
         if(isset($_SESSION["username"])) {
             echo '<li><label for="btn-2" class="show">MédicoBot +</label>
@@ -58,28 +71,44 @@
         <li>
             <label for="btn-2" class="show">MédicoBot +</label>
 >>>>>>> d9a6817 (made navbar based on if session exists (help me))
+=======
+        <?php
+        if(isset($_SESSION["username"])) {
+            echo '<li><label for="btn-2" class="show">MédicoBot +</label>
+>>>>>>> 31cd901 (changes updated navbar)
             <a href="#produits">MédicoBot <i class="fas fa-caret-down"></i></a>
             <input type="checkbox" id="btn-2">
             <ul>
                 <li><a href="#">Vos analyses</a></li>
                 <li><a href="#">Votre dossier</a></li>
 <<<<<<< HEAD
+<<<<<<< HEAD
             </ul></li>';
         } else {
             echo '<li><a href="http://nurse-medicobot.wstr.fr/views/medicalbot/medicobot.php">Médicobot</a></li>';
+=======
+            </ul></li>';
+        } else {
+            echo '<li><a href="#medicobot">Médicobot</a></li>';
+>>>>>>> 31cd901 (changes updated navbar)
         }
         ?>    
         <?php
         if(isset($_SESSION["username"])) {
             echo '<li>
                     <label for="btn-3" class="show">Ecologie +</label>
+<<<<<<< HEAD
                     <a href="#eco">Ecologie <i class="fas fa-caret-down"></i></a>
+=======
+                    <a href="eco">Ecologie <i class="fas fa-caret-down"></i></a>
+>>>>>>> 31cd901 (changes updated navbar)
                     <input type="checkbox" id="btn-3">
                     <ul>
                         <li><a href="#">Votre arbre</a></li>
                     </ul>
                 </li>';
         } else {
+<<<<<<< HEAD
             echo '<li><a href="#ecologie">Ecologie</a></li>';
         }
         ?>
@@ -99,6 +128,11 @@
             </ul>    
         </li>      
         <li><a href="#questions">Ecologie</a></li>
+=======
+            echo '<li><a href="#questions">Ecologie</a></li>';
+        }
+        ?>
+>>>>>>> 31cd901 (changes updated navbar)
         <li><a href="#apropos">A propos</a></li>
         <?php
         if(isset($_SESSION["username"])) {
