44982299259f0e72ab7519a264f4a90ba06d3121
Sh0lf <vyvincentyap@gmail.com>
2022-12-15 16:58:29 +0100

    small changes on the hrefs

--- HTML-CSS/Views/navbar/footer.php
+++ HTML-CSS/Views/navbar/footer.php
@@ -1,6 +1,6 @@
 <div class="bottombar">
     <a href="#cgu">CGU</a>
-    <a href="../misc/FAQ.php">FAQ</a>
-    <a href="../misc/apropos.php">A propos de</a> 
+    <a href="Views/misc/FAQ.php">FAQ</a>
+    <a href="Views/misc/apropos.php">A propos de</a> 
     <a href="#contact">Contact</a>
 </div>
