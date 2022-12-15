36fe336c8914beeef2c8292f64c031bd1ae6c848
Sh0lf <vyvincentyap@gmail.com>
2022-12-15 15:05:42 +0100

    updates css styling + pages

--- HTML-CSS/Views/misc/apropos.php
+++ HTML-CSS/Views/misc/apropos.php
@@ -4,7 +4,11 @@
         <meta charset="utf-8"/>
         <meta http-equiv="refresh" content="30">
         <meta content="width=device-width, initial-scale=1.0">
+<<<<<<< HEAD
         <title>Notre histoire</title>
+=======
+        <title>notre histoire</title>
+>>>>>>> 8044b22 (updates css styling + pages)
         <link rel="stylesheet" href ="../navbar/navbar-main.css">
         <link rel="stylesheet" href ="apropos.css">
 
@@ -17,6 +21,7 @@
       ?>
     </header>
 
+<<<<<<< HEAD
     <div class="container">
       <!--Définition pour chaque partie du paragraphe et de l'image-->
         <h1 class="titre">Qui sommes-nous ?</h1>
@@ -72,6 +77,77 @@
             </div>
         </div>
     </div>
+=======
+        <!-- Section "Qui sommes-nous" -->
+
+        <div class="paragraphe">
+            <h1>Qui sommes-nous ?</h1>
+            <p>Nurse-E, entreprise spécialisée dans le domaine médical.
+        Nous avons pour objectif de développer des outils permettant de faciliter la vie des patients et des médecins.
+        Médicobot, notre produit principal, est une plateforme de soin intelligente capable de diagnostiquer des maladies bénignes. Il est 
+        capable de prescrire des médicaments en cas de faible danger, et , en cas de nécéssité, de contacter un médecin. Médicobot, 
+        intermédiaire efficace et sécurisé entre les patients et les médecins, est donc un outil nécéssaire à un diagnostique  adapté et fiable.
+        </p>
+            
+                <span class="dot"></span>
+                <span class="dot"></span>
+                <span class="dot"></span>
+                <span class="dot"></span>
+                <span class="dot"></span>
+                <span class="dot"></span>
+                <span class="dot"></span>
+                <span class="dot"></span>
+                <span class="dot"></span>
+                <span class="dot"></span>
+                <span class="dot"></span>
+
+        </div>
+
+
+       <!-- Section "Notre histoire" -->
+
+        <div class="paragraphe">
+            <h1>Notre histoire</h1>
+            <p>
+        La récente crise sanitaire a montré à quel point le monde médical est important et à quel point l’humain est fragile. 
+        Durant cette crise sanitaire mondiale, les métiers de la santé ont  été sur sollicités dans la gestion de cette crise. 
+        De nombreux malades, peu gravement atteints, n’ont alors pas été pris en charge par manque de temps et de personnel. 
+        Il est donc  devenu essentiel d’améliorer les conditions d’exercice et l’efficacité des diagnostiques. 
+        C'est pourquoi chez Nurse-E nous nous donnons pour objectif de d’améliorer la situation du monde médical, en mettant en place une plateforme de soin intelligente, Médicobot, capable de diagnostiquer des maladies bénignes, de prescrire des médicaments en cas de faible danger, et de contacter un médecin en cas de nécessité. 
+        Utiliser Médicobot c’est donc faire gagner du temps aux médecins et aux clients, tout en améliorant l’efficacité et la rapidité des diagnostiques.
+        </p>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+
+        </div>
+
+        <!-- Section "Nos valeurs" -->
+        <h1>Nos valeurs</h1>
+
+        <div class="row">
+            <div class="column">
+                <h2><span>Simplicité</span></h2>
+                <p>Nos outils sont ergonomiques et faciles à utiliser.</p>  
+            </div>
+            <div class="column">
+                <h2><span>Accessibilté</span></h2>
+                <p>Nos outils sont offerts à tous les médecins  et clients.</p>           
+            </div>
+            <div class="column">
+                <h2><span>Sécurité</span></h2>
+                <p>Nous garentissons la sécurité de nos outils à tous nos utilisateurs.</p>                      
+            </div>
+        </div>
+>>>>>>> 8044b22 (updates css styling + pages)
         
     <footer>
         <?php
