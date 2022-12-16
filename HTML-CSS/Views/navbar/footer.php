6c33266c600ff8d0675319d7e257ea1c748b453e
Sh0lf <vyvincentyap@gmail.com>
2022-12-16 01:13:29 +0100

    updates...

--- HTML-CSS/Views/navbar/footer.php
+++ HTML-CSS/Views/navbar/footer.php
@@ -1,5 +1,5 @@
 <div class="bottombar">
-    <a href="#cgu">CGU</a>
+    <a href="//localhost:8888/Views/misc/cgu.php">CGU</a>
     <a href="//localhost:8888/Views/misc/FAQ.php">FAQ</a>
     <a href="//localhost:8888/Views/misc/apropos.php">A propos de</a> 
     <a href="#contact">Contact</a>
