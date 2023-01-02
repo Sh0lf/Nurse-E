34f6f20a161151f019f1de65211642d3ce544fcf
Sh0lf <vyvincentyap@gmail.com>
2023-01-02 19:01:06 +0100

    Contact us is done, email system working

--- HTML-CSS/index.php
+++ HTML-CSS/index.php
@@ -1,8 +1,34 @@
 <?php
-
 session_start();
 if (isset($_SESSION["iduser"])){
+    $role = $_SESSION["role"];
 
+    switch($role){
+
+        case 'client':
+            $view = 'client';
+            break;
+
+        case 'medecin':
+            $view = 'medecin';
+            break;
+
+        case 'admin':
+            $view = 'admin';
+            break;
+        
+        default:
+            $view = 'error404';
+            break;
+    }
+    if ($view != "error404"){
+        include DIR ('/views/personalspace/'.$view.'/'.$view.'-personalspace.php');
+    } else {
+        include DIR('/views/error404.php');
+    }
+    
+} else {
+    include DIR ('/views/mainmenu.php');
 }
 
 ?>
