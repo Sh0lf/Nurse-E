43e9f326c4730065789fbedaaeab9eddcdff80c3
Sh0lf <vyvincentyap@gmail.com>
2023-01-02 10:54:26 +0100

    update login sys

--- HTML-CSS/Controller/resetpwd.inc.php
+++ HTML-CSS/Controller/resetpwd.inc.php
@@ -5,15 +5,28 @@ include_once 'functions.inc.php';
 include_once '../model/SQL-loginsystem.php';
 
 if (isset($_POST["submit_pwd"])){
-    $newpwd = $_POST["pwd"];
+    $newpwd = test_input($_POST["pwd"]);
+    $newpwdrep = test_input($_POST["pwdrep"]);
     $code = $_POST["code"];
     error_log(print_r($code, TRUE));
+
+    if (pwdMatch($newpwd, $newpwdrep) !== false){
+        header("location: ../views/loginsys/resetpwd.php?error=pwdmissmatch&code='.$code.'");
+        exit();
+    }
+
+    if (pwdStrengthChecker($newpwd) !== false){
+        header("location: ../views/loginsys/resetpwd.php?error=pwdstrength&code='.$code.'");
+        exit();
+    }
+    
     $result = changePwd($conn, $newpwd, $code);
 
     if ($result == true){
         header("location: ../views/loginsys/login.php?error=remadepwd");
         exit();
     }
+
 } else {
     header("location: ../views/loginsys/login.php");
     exit();
