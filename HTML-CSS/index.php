dedf4f65d656c397f19f932467b4bfb65d91d060
Sh0lf <vyvincentyap@gmail.com>
2023-01-02 19:01:06 +0100

    Contact us is done, email system working

--- HTML-CSS/index.php
+++ HTML-CSS/index.php
@@ -1,5 +1,6 @@
 <?php
 <<<<<<< HEAD
+<<<<<<< HEAD
 ini_set('display_errors', 1);
 session_start();
 if (isset($_SESSION["iduser"])){
@@ -36,9 +37,38 @@ if (isset($_SESSION["iduser"])){
 }
 =======
 
+=======
+>>>>>>> 34f6f20 (Contact us is done, email system working)
 session_start();
 if (isset($_SESSION["iduser"])){
+    $role = $_SESSION["role"];
 
+    switch($role){
+
+        case 'client':
+            $view = 'client';
+            break;
+
+        case 'medecin':
+            $view = 'medecin';
+            break;
+
+        case 'admin':
+            $view = 'admin';
+            break;
+        
+        default:
+            $view = 'error404';
+            break;
+    }
+    if ($view != "error404"){
+        include DIR ('/views/personalspace/'.$view.'/'.$view.'-personalspace.php');
+    } else {
+        include DIR('/views/error404.php');
+    }
+    
+} else {
+    include DIR ('/views/mainmenu.php');
 }
 
 >>>>>>> dc79c0f (trying to implement index.php)
