f4539a74edfea3d840d1ce4108acee604e0d098b
Sh0lf <vyvincentyap@gmail.com>
2023-01-04 00:42:19 +0100

    Updated for web host

--- HTML-CSS/Controller/dbh.inc.php
+++ HTML-CSS/Controller/dbh.inc.php
@@ -16,8 +16,12 @@ $conn = new PDO( 'mysql:host=localhost;dbname=Projet_BDD_APP', $db_username, $db
 =======
 $db_username = 'rflzfr_nursemed_db';
 $db_password = 'M71b-VLo8ls*%9!d';
+<<<<<<< HEAD
 $conn = new PDO( 'mysql:host=176.31.132.185;dbname=Projet_BDD_APP', $db_username, $db_password );
 >>>>>>> 3ec4b8c (added time restrictions)
+=======
+$conn = new PDO( 'mysql:host=176.31.132.185;dbname=rflzfr_nursemed_db', $db_username, $db_password );
+>>>>>>> 13556d4 (Updated for web host)
 if(!$conn){
     die("Fatal Error: Connection Failed!");
 }
