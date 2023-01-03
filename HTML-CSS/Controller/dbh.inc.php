13556d439a9091cef12b0f26b120dea8571b2716
Sh0lf <vyvincentyap@gmail.com>
2023-01-04 00:42:19 +0100

    Updated for web host

--- HTML-CSS/Controller/dbh.inc.php
+++ HTML-CSS/Controller/dbh.inc.php
@@ -2,7 +2,7 @@
 
 $db_username = 'rflzfr_nursemed_db';
 $db_password = 'M71b-VLo8ls*%9!d';
-$conn = new PDO( 'mysql:host=176.31.132.185;dbname=Projet_BDD_APP', $db_username, $db_password );
+$conn = new PDO( 'mysql:host=176.31.132.185;dbname=rflzfr_nursemed_db', $db_username, $db_password );
 if(!$conn){
     die("Fatal Error: Connection Failed!");
 }
