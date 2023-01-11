85ce8c50952b315b6b41c1a30b9d73cc9207da5f
Sh0lf <80401462+Sh0lf@users.noreply.github.com>
2023-01-11 19:05:14 +0100

    Add files via upload

---
+++ HTML-CSS/index.php
@@ -0,0 +1,36 @@
+<?php
+ini_set('display_errors', 1);
+session_start();
+if (isset($_SESSION["iduser"])){
+    $role = $_SESSION["role"];
+
+    switch($role){
+
+        case 'client':
+            $view = 'client';
+            break;
+
+        case 'medecin':
+            $view = 'medecin';
+            break;
+
+        case 'admin':
+            $view = 'admin';
+            break;
+        
+        default:
+            $view = 'error404';
+            break;
+    }
+    if ($view != "error404"){
+        header ('location: https://nurse-medicobot.wstr.fr/views/personalspace/'.$view.'/'.$view.'-personalspace.php');
+        exit();
+    } else {
+        header ("location: views/error404.php");
+    }
+    
+} else {
+    header("location: /views/mainmenu.php");
+    exit();
+}
+?>
