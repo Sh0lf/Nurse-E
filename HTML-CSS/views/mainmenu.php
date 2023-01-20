6671b55f4e16d0ef70ade0169fc491d6607980ad
ClemLaQueen <82205431+ClemLaQueen@users.noreply.github.com>
2023-01-20 12:57:37 +0100

    Nouvelle page d'accueil

--- HTML-CSS/views/mainmenu.php
+++ HTML-CSS/views/mainmenu.php
@@ -9,6 +9,12 @@
     <link rel ="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"> <!-- Permet de prendre des images du site sans avoir à les télécharger -->
     <link rel="stylesheet" href="styleaccueil.css">
     <link rel ="stylesheet" href="navbar/navbar-main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="medeco.css">
     <link rel="shortcut icon" href="/views/assets/Logo-medicobot.png" />
   </head>
 
@@ -20,87 +26,205 @@
       ?>
     </header>
 
-    <div class="contact"> 
-      <h1>Contact</h1><br>
-      <h3>numéro 1</h3><br>
-      <h3>numéro 2</h3><br>
-      <h1>Localisation</h1><br>
-      <div style="width: 100%"><iframe width="200" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=200&amp;height=300&amp;hl=en&amp;q=28%20Rue%20Notre%20Dame%20des%20Champs,%2075006%20Paris+(ISEP)&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/distance-area-calculator.html">measure area map</a></iframe></div>
-    </div>
- 
-    <p class="p1"><a id="home">A cause de la crise du covid, les médecins ainsi que les hôpitaux furent débordés.
-        Depuis cette crise, les rendez-vous inutiles chez les médecins se multiplient.
-        De ce fait, nous avons décidé de créer un produit afin de remédier à ça.
-        Ce produit est MédicoBot, c’est le premier produit créé par notre entreprise Nurse-E.
-      
-        MedicoBot s’apprête à révolutionner le domaine du monde médical
-        comme vous ne l’avez jamais vu.</a></p>
-    
-    <h2>
-        <a id="comment">Comment ça marche ?</a>
-    </h2>
-
-    <p class ="p2">
-    Ces kits vous permettront de mesurer votre température, battement de cœur, etc.
-    Lorsque vous ne vous sentez pas bien, vous pourrez mesurer toutes ces fonctions grâce à notre kit.
-    Vous aurez également des indications de choses à faire de vous-mêmes.
-    Une fois tout celà fait, vous entrez vos résultats et notre site pourra ensuite essayer de vous indiquer ce que vous avez,
-    si c'est bénin, les médicaments à prendre ainsi que pour les cas les plus graves, vous prendre un rdv chez le médecin.
-    Le but de notre produit est donc d'éviter les consultations bateaux chez le médecin afin d'alléger leur emploi du temps
-    mais aussi d'éviter les longs trajets pour aller voir un médecin en campagne.
-    Notre site contient également toute une partie écologie(souligné qui emmène à la page) qui relate les dernières news liées à cela.
-    Nous nous engageons aussi à respecter les enjeux climatiques actuels et pour se faire déjà,
-    dès la création de votre compte, un arbre sera planté ayant votre nom.
-    Vous pourrez ensuite suivre la poussée de cette arbre chaque jour.
-    Ensuite, nous avons un calculateur de CO2, celui-ci vous indiquera le CO2 que vous avez économisé
-    en évitant les déplacements chez un médecin (très intéressant pour les gens en campagne principalement)
-    </p>
- 
-    <h2>
-        <a id="produit">Notre produit</a>
-    </h2>
-    <div class="produit">
-        <img src="exempleproduit.jpg" alt="exemple produit" style="width:350px;height:350px;" >
-    </div>
- 
     <div class="container">
-      <input class="favorite styled"
-            type="button"
-            value="199 $">
-    </div>
- 
-    <h2>
-        <a id="updates">Les Updates </a>
-    </h2>
-    <p class ="p2">
-    Les spawners n’ont plus de type de mob par défaut lorsqu’ils sont placés par un joueur (auparavant, c’était le cochon).
-    N’émet pas de particules de feu lorsqu’un type d’apparition de mob n’a pas été défini.
-    Renommage du Monster Spawner pour correspondre à la Bedrock, et suppression de la couleur violette du texte.
-    Le Pick-block fonctionne désormais pour les blocs Spawner
-    Le type de mob est maintenant affiché dans la description du survol d’une pile d’éléments de Spawner.
-    Si un type de mob n’a pas encore été défini, la description du survol décrira comment le définir.
-    Ajout de nouveaux œufs de ponte pour les monstres Ender Dragon, Iron Golem, Snow Golem et Wither en mode Créatif.
-    Les œufs d’Ender Dragon et de Wither ne seront disponibles que par le biais de commandes afin d’éviter la destruction accidentelle des constructions des joueurs.
-    Les couleurs de l’œuf de ponte de l’ours polaire ont changé pour le distinguer de l’œuf de ponte du fantôme.
-    Les bruits de pas peuvent maintenant être entendus en marchant dessus :
-    tapis
-    nénuphars
-    petits amas d’améthyste
-    Les bruits de pas peuvent maintenant être entendus lorsque l’on marche à travers :
-    germes du Nether
-    lichen lumineux
-    racines carmin
-    racines biscornues
-    </p>
-    <h2>
-        <a id="questions">Vos questions </a>
-    </h2>
-    <div class="contact_bis"> <h1>Contact</h1><br>
-      <h3>numéro 1</h3><br>
-      <h3>numéro 2</h3><br>
-      <h1>Localisation</h1><br>
-      <div style="width: 100%"><iframe width="200" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=200&amp;height=300&amp;hl=en&amp;q=28%20Rue%20Notre%20Dame%20des%20Champs,%2075006%20Paris+(ISEP)&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/distance-area-calculator.html">measure area map</a></iframe></div>
-    </div>
+            <img src="testaccueil.png" style="width:100%;">
+            <h1 class="title-over">MédicoBot</h1>
+            <p class="text-over">A cause de la crise du covid, les médecins ainsi que les hôpitaux furent débordés. 
+            Depuis cette crise, les rendez-vous inutiles chez les médecins se multiplient. De ce fait, nous avons 
+            décidé de créer un produit afin de remédier à ça. Ce produit est MédicoBot, c’est le premier produit 
+            créé par notre entreprise Nurse-E. MedicoBot s’apprête à révolutionner le domaine du monde médical 
+            comme vous ne l’avez jamais vu.</p>
+            <button class="btn-33"><span>Notre produit</span></button>
+            <button class="btn-35"><span>A propos</span></button>
+        </div>
+        <h1 class="titre" style="color:#274359;">Notre produit</h1>
+        <div class="produitvC">
+            <h2 style="margin-left:-125px;color:#274359;">Notre capteur</h2>
+            <p style="margin-left:-245px; color:#274359;">$199.95</p>
+            <img src="capteur.jpg" style="width:300px;">
+        </div>
+        <div class="buttons">
+            <button style="text-transform: uppercase;
+            font-weight: bold;
+            font-family: sans-serif;
+            letter-spacing: 0.1em;" class="fill">Comment ça marche ?</button>
+        </div>
+        <div class="medeco">
+          <div class="cube">
+              <div class="a"></div>
+              <div class="b"></div>
+              <div class="c"></div>
+              <div class="d"></div>
+              <div class="e"></div>
+              <div class="f"></div>
+              <div class="g"></div>
+              <div class="h"></div>
+              <div class="i"></div>
+              <div class="j"></div>
+              <div class="k"></div>
+              <div class="l"></div>
+              <div class="m"></div>
+              <div class="n"></div>
+              <div class="o"></div>
+              <div class="p"></div>
+              <div class="q"></div>
+              <div class="r"></div>
+              <div class="s"></div>
+              <div class="t"></div>
+          </div>
+          <div class="medi">
+              <div class="bl" style="box-shadow: 0px 5px 25px #43B1F8;">
+                <div class="medico">
+                    <img src="logo.png" alt="medicobot">
+                </div>
+                <div class="hh">
+                  <div class="pro">
+                      <div class="produit">
+                          <h1 style="text-transform: uppercase;
+                          font-weight: bold;
+                          font-family: sans-serif;
+                          letter-spacing: 0.1em;">Medicobot</h1>
+                          <h3 style="text-decoration: none;">Votre medecin generaliste de poche</h3>
+                          <p class="p1">Chez Medicobot nous vous offrons un medecin generaliste directement dans votre maison.
+                          Chez Medicobot nous vous offrons un medecin generaliste directement dans votre maison.</p>        
+                          <p class="p1">Acceder a vos analyse :</p>
+                          <button class="btn-38"><span><a href="page_annexe\analyse.html" style="text-decoration: none; color:white;">Commencer une analyse</a></span></button>
+                      </div>
+                  </div>
+                </div>
+              </div>
+            <div class="ve" style="background-color: #3A9D23; box-shadow: 0px 5px 25px #3A9D23;">
+              <div class="ecolo" >
+                  <img src="nurse.png" alt="nurseE">
+              </div> 
+              <div class="hh">
+                      <div class="produit" style="margin-left: -35vw;">
+                              <h1 style="text-transform: uppercase;
+                              font-weight: bold;
+                              font-family: sans-serif;
+                              letter-spacing: 0.1em;">Nurse E</h1>
+                              <h3 style="text-decoration: none;">votre medecin generaliste de poche</h3>
+                              <p class="p1">Chez Medicobot nous vous offrons un medecin generaliste directement dans votre maison.
+                              Chez Medicobot nous vous offrons un medecin  generaliste directement dans votre maison.</p>
+                              <p class="p1">Acceder a vos analyse :</p>
+                              <button class="btn-34"><span><a href="page_annexe\analyse.html" style="text-decoration: none; color:white;">Commencer une analyse</a></span></button>
+                      </div>          
+              </div> 
+            </div>
+          </div>
+        </div>
+          <div class="cub">
+              <div class="A"></div>
+              <div class="B"></div>
+              <div class="C"></div>
+              <div class="D"></div>
+              <div class="E"></div>
+              <div class="F"></div>
+              <div class="G"></div>
+              <div class="H"></div>
+              <div class="I"></div>
+              <div class="J"></div>
+              <div class="K"></div>
+              <div class="L"></div>
+              <div class="M"></div>
+              <div class="N"></div>
+              <div class="O"></div>
+              <div class="P"></div>
+              <div class="Q"></div>
+              <div class="R"></div>
+              <div class="S"></div>
+              <div class="T"></div>
+          </div>
+      <br>
+      <br>
+      <br>
+        <div class="white">
+          <div class="qui">
+              <h1 style="color:#274359;">Qui sommes-nous ?</h1>
+          </div>
+          <div class="container3">
+              <div class="box">
+                <div class="imgBx">
+                  <img src="Vincent.jpg">
+                </div>
+                <div class="content">
+                  <div>
+                    <h2>Vincent YAP</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus molestias quidem iusto.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="box">
+                <div class="imgBx">
+                  <img src="Junior.jpg">
+                </div>
+                <div class="content">
+                  <div>
+                    <h2>Junior YAMEUNDJEU TITCHEU</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus molestias quidem iusto.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="box">
+                <div class="imgBx">
+                  <img src="Clem.jpg">
+                </div>
+                <div class="content">
+                  <div>
+                    <h2>Clementine DEBEUGNY</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus molestias quidem iusto.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="box">
+                <div class="imgBx">
+                  <img src="Leo.jpg">
+                </div>
+                <div class="content">
+                  <div>
+                    <h2>Léo ZERBIB</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus molestias quidem iusto.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="box">
+                <div class="imgBx">
+                  <img src="Marie-Eve.jpg">
+                </div>
+                <div class="content">
+                  <div>
+                    <h2>Marie-Evangéline DE LA BROISE</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus molestias quidem iusto.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="box">
+                <div class="imgBx">
+                  <img src="Aelig.jpg">
+                </div>
+                <div class="content">
+                  <div>
+                    <h2>Aëlig VILLIETTE</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi accusamus molestias quidem iusto.
+                    </p>
+                  </div>
+                </div>
+              </div>
+          </div>
+        <br>
+        <br>
+        </div>
+        <div class="container">
+          <img src="image1.png" style="width:100%;">
+          <h1 class="new-title-over">Rejoignez-nous<br>
+            maintenant !</h1>
+            <button class="btn-36"><span>Inscription</span></button>
+        </div>
     
     <footer>
       <!--Defining our bottom navigation bar in footer, for aesthetics purpose-->
