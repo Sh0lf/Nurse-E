26ef1d879711a135e99d6ce49d50f92a9407a76f
Leozerbib <116356061+Leozerbib@users.noreply.github.com>
2023-06-21 10:22:04 +0200

    Update fetchTomcat.php

--- controller/fetchTomcat.php
+++ controller/fetchTomcat.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
-
-<html>
 <?php
 include_once 'dbh.inc.php';
+
+include_once '../model/SQL-capteur.php';
+
+
 
 // 1. Récupérer les données brutes
 $ch = curl_init();
@@ -14,6 +16,15 @@ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $data = curl_exec($ch);
 curl_close($ch);
 
+
+//echo "Raw Data:<br />";
+//echo "$data";
+
+// 2. Les mettre sous forme de tableau (1 ligne = 1 trame d'un capteur)
+$data_tab = str_split($data, 33);
+//echo "<br /><br />Tabular Data:<br />";
+for ($i = count($data_tab) - 4; $i < count($data_tab) - 1; $i++) {
+    changeFormat($conn, $data_tab[$i]);
 echo "Raw Data:<br />";
 echo "$data";
 
@@ -22,6 +33,7 @@ $data_tab = str_split($data, 33);
 echo "<br /><br />Tabular Data:<br />";
 for ($i = count($data_tab) - 4; $i < count($data_tab) - 1; $i++) {
     echo "\ntrame $i: " . changeFormat($conn, $data_tab[$i]) . "<br>";
+
 }
 
 
@@ -37,42 +49,10 @@ function changeFormat($conn, $dataRow)
     $infos = "type: $t, objet: $o, requête: $r, type de capteur: $c, numéro de capteur: $n, valeur: " . hexdec($v) .
         ", date: $day/$month/$year, $hour:$min:$sec <br>";
     addVal($conn, hexdec($c), hexdec($v));
-    return fetchValeur($conn, hexdec($c));
 }
+$Capteur1=fetchValeur($conn, 10);
+$Capteur2=fetchValeur($conn, 4);
+$Capteur3=fetchValeur($conn, 3);
 
-function addVal($conn, $typcapteur, $valeur)
-{
-    $sql = "INSERT INTO Valeur SET Valeur = ?, idCapteur = ?";
-
-    $stmt = $conn->prepare($sql);
-    if (!$stmt) {
-        header("location: ../views/personalspace/admin/adminfaq-personalspace.php?error=stmtfailed");
-        exit();
-    }
-    $stmt->execute(array($valeur, $typcapteur));
-}
-
-function fetchValeur($conn, $idcapt)
-{
-    $sql = "SELECT Valeur FROM Valeur WHERE idcapteur = ? ORDER BY idvaleur DESC LIMIT 3";
-    
-    $stmt = $conn->prepare($sql);
-    if (!$stmt) {
-        header("location: ../views/personalspace/admin/adminfaq-personalspace.php?error=stmtfailed");
-        exit();
-    }
-    $stmt->execute(array($idcapt));
-    $fetchedRow = $stmt->fetch();
-    return $fetchedRow['Valeur'];
-}
 ?>
-<body>
-    <div>
-        <div class="container1"><?php echo fetchValeur($conn, 10);?></div>
-        <div class="container2"><?php echo fetchValeur($conn, 4);?></div>
-        <div class="container3"><?php echo fetchValeur($conn, 3);?></div>
-        <div class="container4"></div>
-    </div>
-</body>
 
-</html>
