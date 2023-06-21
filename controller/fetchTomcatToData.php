4cbb7b80b3382946ea4ca761195e183b5f75f4b9
Sh0lf <vyvincentyap@gmail.com>
2023-06-21 14:41:28 +0200

    Some changes to bug fix

--- controller/fetchTomcatToData.php
+++ controller/fetchTomcatToData.php
@@ -15,41 +15,27 @@ curl_close($ch);
 //echo "Raw Data:<br />";
 //echo "$data";
 
-// 2. Les mettre sous forme de tableau (1 ligne = 1 trame d'un capteur)
 $data_tab = str_split($data, 33);
 //echo "<br /><br />Tabular Data:<br />";
 for ($i = count($data_tab) - 4; $i < count($data_tab) - 1; $i++) {
     changeFormat($conn, $data_tab[$i]);
 }
-echo "Raw Data:<br />";
-echo "$data";
 
-// 2. Les mettre sous forme de tableau (1 ligne = 1 trame d'un capteur)
-$data_tab = str_split($data, 33);
-echo "<br /><br />Tabular Data:<br />";
-for ($i = count($data_tab) - 4; $i < count($data_tab) - 1; $i++) {
-    echo "\ntrame $i: " . changeFormat($conn, $data_tab[$i]) . "<br>";
+$result1 = fetchValeur($conn, 3);
+$Capt1 = $result1["Valeur"];
+$result2 = fetchValeur($conn, 4);
+$Capt2 = $result2["Valeur"];
+$result3 = fetchValeur($conn, 10);
+$Capt3 = $result3["Valeur"];
 
-}
-
-$Capt1 = fetchValeur($conn, 3);
-$Capt2 = fetchValeur($conn, 4);
-$Capt3 = fetchValeur($conn, 10);
-
-header('location: ../views/personalspace/client/capteurs.php?capt1='.$Capt1.'&capt2='.$Capt2.'&capt3='.$Capt3);
+header('location: /views/personalspace/client/capteurs.php?capt1='.$Capt1.'&capt2='.$Capt2.'&capt3='.$Capt3);
 exit();
 
 // 3. Décoder 1 trame
 function changeFormat($conn, $dataRow)
 {
     $trame = $dataRow;
-    $trame_type = substr($trame, 0, 1);
-    $object_num = hexdec(substr($trame, 1, 10));
-    
     list($t, $o, $r, $c, $n, $v, $a, $x, $year, $month, $day, $hour, $min, $sec) = sscanf($trame, "%1s%4s%1s%1s%2s%4s%4s%2s%4s%2s%2s%2s%2s%2s");
-
-    $infos = "type: $t, objet: $o, requête: $r, type de capteur: $c, numéro de capteur: $n, valeur: " . hexdec($v) .
-        ", date: $day/$month/$year, $hour:$min:$sec <br>";
     addVal($conn, hexdec($c), hexdec($v));
 }
 ?>
